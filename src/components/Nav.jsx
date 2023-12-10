7695fdc977f460eedbf4d13113bc41e5b1100d86
Rina Mendoza <rinamendozaa@gmail.com>
2023-12-10 10:38:41 -0500

    initial commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const Nav = () => {
+  return (
+    <div>
+      <nav>
+        <div>
+          <img src="winnerLogo.png" alt="Winner Partners Logo" />
+        </div>
+        <div>
+          <div>Home</div>
+          <div>About</div>
+          <div>About</div>
+          <div>About</div>
+          <div>About</div>
+          <div>About</div>
+          <div>About</div>
+        </div>
+      </nav>
+    </div>
+  );
+};
